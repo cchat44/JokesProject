578e4d8981facc8979afd26af5bf92c66c080c41
Cory Chatman <cc4chat@gmail.com>
2022-09-06 18:28:42 -0400

    pages added

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const container = document.getElementsByClassName('results');
-const containerWithId = document.getElementById('results');
 
-
-
-function getJokes() {
-    console.log("button is working");
-}
 
 
 
