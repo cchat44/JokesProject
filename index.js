6fcfce1c7be7416266300cca95580f7116b9fd80
Cory Chatman <cc4chat@gmail.com>
2022-09-12 20:43:33 -0400

    home page changes

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const jokeDiv = document.querySelector(".container .joke p");
 
 document.addEventListener("DOMContentLoaded", getJock);
 
-button.addEventListener("click",getJock);
-async function getJock() {
+button.addEventListener("click",getJoke);
+async function getJoke() {
   const jokeData = await fetch("https://icanhazdadjoke.com/", {
     headers: {
       Accept: "application/json"
